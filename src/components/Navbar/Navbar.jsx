b470cd2f25f3e0da95b4bbfad07a5a36b3230557
Mohammad Ali <aliuser1129@gmail.com>
2026-04-10 17:51:00 +0530

    Created the frontend part

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import {assets} from '../../assets/assets'
+import {Link} from 'react-router-dom'
 const Navbar = () => {
+
+    const [menu,setMenu] = useState("menu")
   return (
     <div className='navbar'>
         <img src={assets.logo} alt="" className="logo" />
         <ul className="navbar-menu">
-            <li>home</li>
-            <li>menu</li>
-            <li>mobile-app</li>
-            <li>contact us</li>
+            <Link to='/' onClick={()=>setMenu("home")} className={menu==="home" ? "active" : ""}>home</Link>
+            <a href = '#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu" ? "active" : ""}>menu</a>
+            <a href = '#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app" ? "active" : ""}>mobile-app</a>
+            <a href = '#footer' onClick={()=>setMenu("contact-us")} className={menu==="contact-us" ? "active" : ""}>contact us</a>
         </ul>
         <div className="navbar-right">
             <img src={assets.search_icon} alt="" />
@@ -20,6 +23,7 @@ const Navbar = () => {
             <button>sign in</button>
         </div>
     </div>
+
   )
 }
 
